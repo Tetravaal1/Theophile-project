6a0def3b9f6c3a3eb0b724eeb335d8871b5c8094
Unknown <hugo.raguin@gmail.com>
2018-11-19 11:13:56 +0100

    Script Boutton

---
+++ Theophile project/Assets/Scripts/Ebutton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ebutton : MonoBehaviour {
+
+    private SpriteRenderer sRenderer;
+    private GameManager gameManager;
+    public Sprite bouttonPresse;
+
+	void Start () {
+        sRenderer = GetComponent<SpriteRenderer>();
+        gameManager = FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            sRenderer.sprite = bouttonPresse;
+        }
+        if(Input.GetKeyUp(KeyCode.E)) {
+            gameManager.Changement();
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Theophile project/Assets/Scripts/PressE.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class PressE : MonoBehaviour {
+
+    public GameObject leboutton;
+    public Vector3 buttonPos;
+    private GameObject buttonE;
+    private Vector3 offset = new Vector3(0.65f, 0.70f, 0f);
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            Debug.Log("boup");
+            buttonE = Instantiate(leboutton, transform.position+offset, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Destroy(buttonE);
+    }
+}
