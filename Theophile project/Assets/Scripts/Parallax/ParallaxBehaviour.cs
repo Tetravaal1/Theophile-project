c6ab1ee5647495e671b91323be0905268506e53a
Marie Mouttet <marie.mouttet@hotmail.fr>
2018-11-21 16:26:28 +0100

    CleanupColor_AddVFX

---
+++ Theophile project/Assets/Scripts/Parallax/ParallaxBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBehaviour : MonoBehaviour {
+
+	[System.NonSerialized]
+	public Transform self;
+
+	public Vector2 speed = Vector2.one * 0.1f;
+
+	[System.NonSerialized]
+	public Vector3 startPos;
+
+	ParallaxManager parallax;
+
+	void Start () {
+		parallax = ParallaxManager.instance;
+		self = transform;
+		startPos = self.position;
+	}
+	
+	void Update () {
+		self.position = startPos + Vector3.left * parallax.progress.x * speed.x + Vector3.down * parallax.progress.y * speed.y;
+	}
+}
---
+++ Theophile project/Assets/Scripts/Parallax/ParallaxManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxManager : MonoBehaviour {
+
+	public static ParallaxManager instance;
+
+	[System.NonSerialized]
+	public Transform player;
+
+	[System.NonSerialized]
+	public Vector2 progress;
+
+	[System.NonSerialized]
+	public Vector3 playerStartPos;
+
+	// Use this for initialization
+	void Awake () {
+		player = transform;
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		progress.x = (player.position - playerStartPos).x; 
+		progress.y = (player.position - playerStartPos).y; 
+	}
+}
