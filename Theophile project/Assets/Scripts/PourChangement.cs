c4f9bced8f912bf0e4a24380e85aa78b567ec5f4
Unknown <hugo.raguin@gmail.com>
2018-11-21 18:33:47 +0100

    jsp

---
+++ Theophile project/Assets/Scripts/PourChangement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public struct Duo {
+
+       public Sprite original;
+       public Sprite nouveau;
+
+    }
